4f9f37313afaeb7b9cb726ae7c3543aeed7be7a8
sanjay <arjunsanjay0@gmail.com>
2024-06-07 18:44:40 +0530

    added homepage with api key input and google btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,55 @@
 "use client";
 
-import React from "react";
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 const Home = () => {
+  const [apiKey, setApiKey] = useState("");
+  const [inputError, setInputError] = useState(false);
+
+  const handleGoogleBtnClick = () => {
+    if (apiKey.trim() === "") {
+      setInputError(true);
+      return;
+    } else {
+      setInputError(false);
+      //Google authentication logic goes here
+    }
+  };
+
   return (
-    <div className="flex items-center justify-center h-screen text-4xl">
-      Home
+    <div className="container max-w-3xl">
+      <nav className="pt-10 text-xl">AI Email Classifier</nav>
+      <div className="flex flex-col items-center h-full my-72 text-4xl gap-10">
+        <div className="w-1/2">
+          <Input
+            className={` ${
+              inputError
+                ? "ring-2 ring-red-600 focus-visible:ring-2 focus-visible:ring-red-600"
+                : ""
+            }`}
+            type="text"
+            placeholder="Enter your OpenAI API Key"
+            value={apiKey}
+            onChange={(e) => setApiKey(e.target.value)}
+          />
+          {inputError && (
+            <p className="text-xs mt-3 ml-1 text-red-600">
+              OpenAI API key is required!
+            </p>
+          )}
+        </div>
+
+        <Image
+          onClick={handleGoogleBtnClick}
+          className="cursor-pointer hover:scale-105 transition shadow"
+          src={"/images/google.svg"}
+          alt="google oauth login button"
+          width={197}
+          height={40}
+        />
+      </div>
     </div>
   );
 };
